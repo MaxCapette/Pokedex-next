4de4e6da42b403fb484c9ce075bdd01be5d6f14e
MaxCapette <sk8baz@gmail.com>
2023-09-12 11:42:23 +0200

    ajout de plein de trucs oups

---
+++ src/components/Card.tsx
@@ -0,0 +1,37 @@
+import { Pokemon } from '@/@types/pokemon';
+import Link from 'next/link';
+import LikeBtn from './LikeBtn';
+
+interface CardProps {
+  pokemon: Pokemon;
+}
+
+function Card({ pokemon }: CardProps) {
+  return (
+    <div className="bg-cyan-900 rounded-lg shadow-lg p-4 flex flex-col w-full hover:bg-cyan-800">
+      <LikeBtn />
+      <Link href={`/pokemon/${pokemon.name.en}`}>
+        <img src={pokemon.sprites.regular} alt={pokemon.name.fr} />
+        <div className="py-2 text-center">
+          <h3 className="font-bold text-cyan-400">
+            {`#${pokemon.pokedexId} - ${pokemon.name.fr}`}
+          </h3>
+          <h4 className="text-cyan-600 text-center">{pokemon.name.en}</h4>
+        </div>
+        <p className="flex flex-wrap gap-2 justify-center">
+          {pokemon.types?.map((type) => (
+            <span
+              key={type.name}
+              className="flex items-center gap-2 text-sm text-slate-300"
+            >
+              {type.name}
+              <img src={type.image} alt={type.name} className="w-6" />
+            </span>
+          ))}
+        </p>
+      </Link>
+    </div>
+  );
+}
+
+export default Card;
